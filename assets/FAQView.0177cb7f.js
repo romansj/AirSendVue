09677a4fa2f4877c102311e6a7a500063d6aeb10
Jānis Romāns <30439620+romansj@users.noreply.github.com>
2022-03-23 15:52:51 +0200

    deploy

---
+++ assets/FAQView.0177cb7f.js
@@ -0,0 +1,278 @@
+import{o as n,b as o,f as p,i as u,H as h,w as a,e,F as s,p as d,v as c,t as m}from"./vendor.271fe6a1.js";import{I as y}from"./ItemSummaryDetails.ab6898c3.js";import"./index.58192b92.js";const w=e("title",null,"Frequently Asked Questions | AirSend",-1),g=e("meta",{name:"title",content:"Download and install AirSend app for Android and Windows devices"},null,-1),A=e("meta",{name:"description",content:"Download and install the latest version of AirSend app for Android and Windows devices on Google Play Store and Microsoft Store."},null,-1),f=e("meta",{name:"keywords",content:"airsend download, airsend install, airsend android, airsend windows, airsend app, clipboard sync, android, windows, share clipboard to mobile, download, easy clipboard sync"},null,-1),S=e("section",null,[e("h1",null,"FAQ"),e("p",null,"Frequently asked questions")],-1),b=e("br",null,null,-1),x={class:"faq_category"},_=["innerHTML"],Q=e("br",null,null,-1);let q=[{q:"TEST Where can I download AirSend? START",a:"downloads[[[[Downloads page[[ is where you can download AirSend"},{q:"TEST Where can I download AirSend? MIDDLE",a:"downloads[[You can download AirSend on the [[Downloads page[[ and see installation instructions as well"},{q:"TEST Where can I download AirSend? END",a:"downloads[[You can download AirSend [[Downloads page"},{q:"Where can I download AirSend?",a:"downloads[[You can download AirSend on the [[Downloads page"},{q:"Why don't you have an exe installer?",a:"https://github.com/romansj/AirSendReleases/releases[[We recommend you use the Microsoft Store app, which is the same exe file, but verified so you know it is safe. However, if you are using Windows 7, you can download the exe file[[here"},{q:"Is AirSend free?",a:"pricing[[AirSend is free to use. If you require more functionality, a paid Pro version is available. See [[Pricing[[ for more details."}],v=[{q:"Does AirSend reconnect to my devices when my phone connects to home wifi?",a:"guide[[Please see [[Troubleshooting guide[[."}],I=[{q:"Is the app coming to Mac?",a:"There is no plan to support Mac OS at this time."},{q:"Is the app coming to iOS?",a:"There is no plan to support iOS at this time."},{q:"Are you going to support Bluetooth?",a:"Bluetooth is planned and in the works. Stay tuned!"}],T=[{q:"I can't connect to another device",a:"guide[[Please see [[Troubleshooting guide[[."},{q:"I can send messages from one device, but not from the other",a:"guide[[Please see [[Troubleshooting guide[[."},{q:"I can't see the Android notification",a:"guide[[Please see [[Troubleshooting guide[[."},{q:"Android app closes on its own",a:"guide[[Please see [[Troubleshooting guide[[."}],k=[{q:"Is my data safe?",a:"Yes. Your data is encrypted and visible only to you."},{q:"What encryption standard do you use?",a:"AES-256"}],t=new Map;t.set("General",q);t.set("Capabilities",v);t.set("OS support",I);t.set("Troubleshooting",T);t.set("Security",k);let P=`
+{
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "name": "Frequently Asked Questions",
+  "mainEntity": [
+    {
+      "@type": "Question",
+      "name": "Where can I download AirSend?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "You can download AirSend on the <a href='https://airsend.dedumets.com/downloads' target='_blank'>Downloads page</a>"
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "Why don't you have an exe installer?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "We recommend you use the Microsoft Store app, which is the same exe file, but verified so you know it is safe. However, if you are using Windows 7, you can download the exe file <a href='https://airsend.dedumets.com/releases' target='_blank'>here</a>"
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "Is AirSend free?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "AirSend is free to use. If you require more functionality, a paid Pro version is available. See <a href='https://airsend.dedumets.com/pricing' target='_blank'>Pricing</a> for more details."
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "Does AirSend reconnect to my devices when my phone connects to home wifi?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Yes. AirSend reconnects to saved devices when network connection is reestablished."
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "Is the app coming to Mac?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "There is no plan to support Mac OS at this time."
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "Is the app coming to iOS?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "There is no plan to support iOS at this time."
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "Are you going to support Bluetooth?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Bluetooth is planned and in the works. Stay tuned!"
+      }
+    }
+    ,
+    {
+      "@type": "Question",
+      "name": "I can't connect to another device",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Please see <a href='https://airsend.dedumets.com/guide' target='_blank'>Troubleshooting guide</a>."
+      }
+    }
+    ,
+    {
+      "@type": "Question",
+      "name": "I can send messages from one device, but not from the other",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Please see <a href='https://airsend.dedumets.com/guide' target='_blank'>Troubleshooting guide</a>."
+      }
+    }
+    ,
+    {
+      "@type": "Question",
+      "name": "I can't see the Android notification",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Please see <a href='https://airsend.dedumets.com/guide' target='_blank'>Troubleshooting guide</a>."
+      }
+    }
+    ,
+    {
+      "@type": "Question",
+      "name": "Android app closes on its own",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Please see <a href='https://airsend.dedumets.com/guide' target='_blank'>Troubleshooting guide</a>."
+      }
+    }
+    ,
+    {
+      "@type": "Question",
+      "name": "Is my data safe?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Yes. Your data is encrypted and visible only to you."
+      }
+    }
+    ,
+    {
+      "@type": "Question",
+      "name": "What encryption standard do you use?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "AES-256"
+      }
+    }
+  ]
+}
+`,W=`
+{
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "name": "General",
+  "mainEntity": [
+    {
+      "@type": "Question",
+      "name": "Where can I download AirSend?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "You can download AirSend on the <a href='https://airsend.dedumets.com/downloads' target='_blank'>Downloads page</a>"
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "Why don't you have an exe installer?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "We recommend you use the Microsoft Store app, which is the same exe file, but verified so you know it is safe. However, if you are using Windows 7, you can download the exe file <a href='https://airsend.dedumets.com/releases' target='_blank'>here</a>"
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "Is AirSend free?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "AirSend is free to use. If you require more functionality, a paid Pro version is available. See <a href='https://airsend.dedumets.com/pricing' target='_blank'>Pricing</a> for more details."
+      }
+    }
+  ]
+}
+`,O=`
+{
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "name": "Operating System Support",
+  "mainEntity": [
+
+    {
+      "@type": "Question",
+      "name": "Is the app coming to Mac?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "There is no plan to support Mac OS at this time."
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "Is the app coming to iOS?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "There is no plan to support iOS at this time."
+      }
+    }
+
+  ]
+}
+`,D=`
+{
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "name": "Capabilities",
+  "mainEntity": [
+
+    {
+      "@type": "Question",
+      "name": "Does AirSend reconnect to my devices when my phone connects to home wifi?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Yes. AirSend reconnects to saved devices when network connection is reestablished."
+      }
+    },
+    {
+      "@type": "Question",
+      "name": "Are you going to support Bluetooth?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Bluetooth is planned and in the works. Stay tuned!"
+      }
+    }
+
+
+  ]
+}
+`,E=`
+{
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "name": "Troubleshooting",
+  "mainEntity": [
+    
+    {
+      "@type": "Question",
+      "name": "I can't connect to another device",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Please see <a href='https://airsend.dedumets.com/guide' target='_blank'>Troubleshooting guide</a>."
+      }
+    }
+    ,
+    {
+      "@type": "Question",
+      "name": "I can send messages from one device, but not from the other",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Please see <a href='https://airsend.dedumets.com/guide' target='_blank'>Troubleshooting guide</a>."
+      }
+    }
+    ,
+    {
+      "@type": "Question",
+      "name": "I can't see the Android notification",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Please see <a href='https://airsend.dedumets.com/guide' target='_blank'>Troubleshooting guide</a>."
+      }
+    }
+    ,
+    {
+      "@type": "Question",
+      "name": "Android app closes on its own",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Please see <a href='https://airsend.dedumets.com/guide' target='_blank'>Troubleshooting guide</a>."
+      }
+    }
+
+
+  ]
+}
+`,M=`
+{
+  "@context": "https://schema.org",
+  "@type": "FAQPage",
+  "name": "Security",
+  "mainEntity": [
+    
+    {
+      "@type": "Question",
+      "name": "Is my data safe?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "Yes. Your data is encrypted and visible only to you."
+      }
+    }
+    ,
+    {
+      "@type": "Question",
+      "name": "What encryption standard do you use?",
+      "acceptedAnswer": {
+        "@type": "Answer",
+        "text": "AES-256"
+      }
+    }
+
+
+  ]
+}
+`,Y=JSON.parse(P),F=[JSON.parse(W),JSON.parse(O),JSON.parse(D),JSON.parse(E),JSON.parse(M)];const B={data(){return{qanda:t,faq:Y,faqAll:F}}},L=Object.assign(B,{setup(N){return(l,H)=>(n(),o(s,null,[p(u(h),null,{default:a(()=>[w,g,A,f]),_:1}),e("div",null,[S,b,e("section",null,[(n(!0),o(s,null,d(l.faqAll,i=>(n(),o("div",x,[e("h3",null,c(i.name),1),(n(!0),o(s,null,d(i.mainEntity,r=>(n(),m(y,null,{heading:a(()=>[e("b",null,c(r.name),1)]),description:a(()=>[e("p",{innerHTML:r.acceptedAnswer.text},null,8,_)]),_:2},1024))),256))]))),256))]),Q])],64))}});export{L as default};
---
+++ assets/GuideView.83138e00.js
@@ -0,0 +1,122 @@
+import{a as _,o,b as i,f as p,i as m,H as w,w as s,e,F as c,p as u,q as v,s as f,g as d,n as y,t as b,v as S}from"./vendor.271fe6a1.js";import{I as T}from"./ItemSummaryDetails.ab6898c3.js";import{_ as H}from"./index.58192b92.js";const t=a=>(v("data-v-14083cae"),a=a(),f(),a),x=t(()=>e("title",null,"Guide | AirSend",-1)),k=t(()=>e("meta",{name:"title",content:"Download and install AirSend app for Android and Windows devices"},null,-1)),I=t(()=>e("meta",{name:"description",content:"Download and install the latest version of AirSend app for Android and Windows devices on Google Play Store and Microsoft Store."},null,-1)),A=t(()=>e("meta",{name:"keywords",content:"airsend download, airsend install, airsend android, airsend windows, airsend app, clipboard sync, android, windows, share clipboard to mobile, download, easy clipboard sync"},null,-1)),C=t(()=>e("section",null,[e("h1",null,"Guide"),e("p",null,"Here you can find a general testing guide as well as troubleshooting steps if you've run into a problem"),e("br"),e("h2",null,"Jump to:"),e("div",{style:{display:"flex","flex-direction":"column"}},[e("a",{href:"#howto"},"How-to"),e("a",{href:"#testing"},"Testing"),e("a",{href:"#troubleshooting"},"Troubleshooting")])],-1)),j=t(()=>e("br",null,null,-1)),O=t(()=>e("h2",{id:"howto"},"How-to guide",-1)),D=["innerHTML"],G=["src"],N=t(()=>e("br",null,null,-1)),q=t(()=>e("br",null,null,-1)),P=t(()=>e("br",null,null,-1)),B=t(()=>e("h2",{id:"testing"},"Testing guide",-1)),M=t(()=>e("p",null," Things to do: Poke around, send messages, connect, disconnect, switch network off/on, share text to the app, paste a very long text and send, write your language's special characters, provide bad data (e.g. scan fake QR) ",-1)),Q=t(()=>e("br",null,null,-1)),R=t(()=>e("p",null," I would be very thankful if you poked around the app, tried out its functionality and gave it a hard time :). When you find some issue, please drop me a message - what happened and what steps did you take to get there. ",-1)),V=t(()=>e("br",null,null,-1)),Y=d(" Get started testing by "),J=d("downloading Airsend"),L=d("today! "),W=y('<br data-v-14083cae><section data-v-14083cae><h2 id="troubleshooting" data-v-14083cae>Troubleshooting guide</h2><p data-v-14083cae> Please write down the last steps you took and drop me a message. That will help me investigate and fix the issue. <br data-v-14083cae>Now, for you to solve the issue: </p><br data-v-14083cae><h3 data-v-14083cae>App not responding:</h3><p data-v-14083cae>Android</p><ul data-v-14083cae><li data-v-14083cae>Swipe up and hold (or press the rectangle button) to see all open apps. Close AirSend.</li><li data-v-14083cae>Close AirSend.</li></ul><p data-v-14083cae>PC</p><ul data-v-14083cae><li data-v-14083cae>Open Task Manager and select AirSendWPF2.exe</li><li data-v-14083cae>Click &#39;End process&#39;</li></ul><br data-v-14083cae><h3 data-v-14083cae>Something not working right:</h3><p data-v-14083cae>Click on Restart button in the app, it should be back to normal.</p><br data-v-14083cae><h3 data-v-14083cae>Other issue:</h3><p data-v-14083cae>Please drop me a message with issue description.</p></section>',2);let F=[{q:"How to connect to a device?",a:`You want to connect from device A to device B.<br><br>
+    On device B:<br>
+    open AirSend<br>
+    on Connections section swipe up or tap 'This device'<br><br>
+    On Device A:<br>
+    on Connections section tap 'Connect'<br>
+    Scan the QR code or enter IP address and port<br><br>
+    All done!
+    `},{q:"How to disconnect a device?",a:"Go to Connections section, long tap on the device and in the menu that appears tap Disconnect"},{q:"How can I get a QR code?",a:"Go to Connections section and swipe up or tap 'This device'"},{q:"How can I see my device's IP address and port number?",a:"Go to Connections section and swipe up or tap 'This device'"}],z=[{q:"How do I disable AirSend from starting automatically?",a:"Open Settings and turn off the toggle 'Start AirSend automatically'"},{q:"How do I disable notifications?",a:"Open Settings and toggle on/off the type of notifications you would like to receive. You will still receive messages even if you turn off the notifications"},{q:"How do I disable battery optimization?",a:"Open Settings and under 'Disable battery optimization' tap 'Disable' and follow the on screen prompts"}],E=[{q:"How do I rename a device?",a:"You are on device A, you see device B and you want to rename it. Open Settings on device B and set the name for the device. You will need to reconnect to see the change on device B"}],U=[{q:"How can I send a message only to one device?",a:"In Messages section, under 'Select recipient' tap the selection box and pick 'Selected devices'. You will be able to select one or many devices. If you already have selected devices before, but would like to change the selection, tap the devices icon that shows to the right of selection box when 'Selected devices' is picked"}],n=new Map;n.set("Connections",F);n.set("Messaging",U);n.set("Devices",E);n.set("Other",z);let K=`
+{
+  "@context": "https://schema.org",
+  "@type": "HowTo",
+  "name": "How to connect to a device",
+  "step": [
+    {
+      "@type": "HowToStep",
+      "text": "Go to Connections page",
+      "image": "/section_connections.jpg"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "Tap 'Connect'",
+      "image": "/connect_device/2_button_connect.jpg"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "The connection dialog will be displayed. You can now scan the other device's QR code or enter IP address and port manually",
+      "image": "/connect_device/3_dialog_connect.jpg"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "On the second device, go to the Connections page as well and tap on 'This device'",
+      "image": "/connect_device/2_button_connect.jpg"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "Now on the first device scan the QR code or enter IP adress and port.",
+      "image": "/connect_device/4_scanned_qr.jpg"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "All done, you're connected!",
+      "image": "/device_list.jpg"
+    }
+  ]
+}
+`,X=`
+{
+  "@context": "https://schema.org",
+  "@type": "HowTo",
+  "name": "How to disconnect a device?",
+  "step": [
+    {
+      "@type": "HowToStep",
+      "text": "Go to Connections page",
+      "image": "/modify_notifications/section_settings.jpg"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "Long tap on the device",
+      "image": "/modify_notifications/2_notification_settings.jpg"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "In the menu that appears tap Disconnect",
+      "image": "/modify_notifications/3_notification_toggled.jpg"
+    }
+  ]
+}  
+`,Z=`
+{
+  "@context": "https://schema.org",
+  "@type": "HowTo",
+  "name": "How can I send a message to only one device?",
+  "step": [
+    {
+      "@type": "HowToStep",
+      "text": "Go to Messages page",
+      "image": "/message_one_device/0_messages_page.jpg"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "Under 'Select recipient' tap the selection box and pick 'Selected devices'",
+      "image": "/message_one_device/2_recipient_choice_selected.jpg"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "In the dialog that appears select one or many devices you would like to message. Tap 'Confirm' after you're done selecting",
+      "image": "/message_one_device/4_dialog_recipient_choice_checked.jpg"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "Write and send your message",
+      "image": "/message_one_device/5_message_typed.jpg"
+    }
+  ]
+}
+`,$=`
+{
+  "@context": "https://schema.org",
+  "@type": "HowTo",
+  "name": "How do I disable notifications on AirSend?",
+  "step": [
+    {
+      "@type": "HowToStep",
+      "text": "Open Settings",
+      "image": "/modify_notifications/section_settings.jpg"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "Scroll down to Notifications page",
+      "image": "/modify_notifications/2_notification_settings.jpg"
+    },
+    {
+      "@type": "HowToStep",
+      "text": "Toggle on/off the type of notifications you would like to receive",
+      "image": "/modify_notifications/3_notification_toggled.jpg"
+    }
+  ]
+}
+
+`,ee=[JSON.parse(K),JSON.parse(X),JSON.parse(Z),JSON.parse($)];const te={data(){return{howToList:n,howToItems:ee}}},oe=Object.assign(te,{setup(a){return(g,ne)=>{const h=_("router-link");return o(),i(c,null,[p(m(w),null,{default:s(()=>[x,k,I,A]),_:1}),e("div",null,[C,j,e("section",null,[O,(o(!0),i(c,null,u(g.howToItems,l=>(o(),b(T,null,{heading:s(()=>[e("b",null,S(l.name),1)]),description:s(()=>[(o(!0),i(c,null,u(l.step,r=>(o(),i("span",null,[e("p",{innerHTML:r.text},null,8,D),e("img",{class:"img_guide_phone",src:r.image},null,8,G),N,q]))),256))]),_:2},1024))),256))]),P,e("section",null,[B,M,Q,R,V,e("p",null,[Y,p(h,{to:"downloads",title:"Download AirSend for your device"},{default:s(()=>[J]),_:1}),L])]),W])],64)}}});var ce=H(oe,[["__scopeId","data-v-14083cae"]]);export{ce as default};
